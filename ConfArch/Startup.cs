e7774dd71797a288d80d5aa895bad4cde40c6473
panda <kasthuri.14@cse.mrt.ac.lk>
2019-11-24 23:35:33 +0530

    00-use conference controller's index action as default action

--- ConfArch/Startup.cs
+++ ConfArch/Startup.cs
@@ -50,7 +50,7 @@ namespace ConfArch
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Conference}/{action=Index}/{id?}");
             });
         }
     }
